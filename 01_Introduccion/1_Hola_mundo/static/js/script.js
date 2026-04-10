5e2f1f73eaea79dc6d65d6e5db50d34e37236606
sashabarrera <sashabarrera@liceovvh.cl>
2026-04-10 08:35:05 -0400

    listo

---
+++ 01_Introduccion/1_Hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+// Prueba de conexion con js
+console.log("Conexion exitosa con js externo...");
+
+// 1) Hola mundo con variable y const.
+const saludo = "Hola a todos, ";
+let destinatario = "estudiantes";
+// Impresion de saludos concatenando con (+)
+console.log(saludo + "para  los " + destinatario);
+
+// 2) Saludar a una persona ingresada por un teclado.
+let nombre = prompt("Ingrese un nombre: ")
+// Imprimir un nombre ingresado con un prompt
+console.log("Hola, "+  nombre)
---
+++ 02_Tipos_de_datos/1-Tipos_textos_de_datos/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("Conexion con js correcta...")
+/*
+Dto. Tipo texto(string)
+1.- Concatenacion de texto con el signo + 
+Podemos unir texto y variables
+*/
+const nombre = "Sasha";
+const apellido = "Barrera";
+//Unimos ambas constantes con un texto extra.
+console.log("Hola, mi nombre es: " + nombre + "" + apellido);
